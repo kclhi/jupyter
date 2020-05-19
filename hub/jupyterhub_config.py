5e57d14f07bb95e3c92f9e729a589a8422b12aca
Martin Chapman <contact@martinchapman.co.uk>
2020-05-19 15:46:45 +0100

    Admin config.

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -1,7 +1,6 @@
 import os, sys
 
 ## Generic
-c.JupyterHub.admin_access = True;
 c.JupyterHub.base_url = "/jupyter";
 c.JupyterHub.trusted_alt_names = ["localhost", "kclhi.org"];
 
@@ -10,6 +9,8 @@ c.JupyterHub.authenticator_class = "ldapauthenticator.LDAPAuthenticator";
 c.LDAPAuthenticator.server_address = "covid-2_openldap_1";
 c.LDAPAuthenticator.bind_dn_template = ["uid={username},dc=kclhi,dc=org"];
 c.LDAPAuthenticator.use_ssl = True;
+c.Authenticator.admin_users = {os.environ.get("ADMIN_USERS")}
+c.JupyterHub.admin_access = True;
 
 ## Docker spawner
 c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner";
